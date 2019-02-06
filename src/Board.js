4dc292305cd434bace5e08ed1dcb66f0c053c24b
Priyadubal18 <priya.dubal18@gmai.com>
2019-02-05 20:16:43 -0500

    Complete board through diagonal conflicts

--- src/Board.js
+++ src/Board.js
@@ -174,12 +174,28 @@
     //
     // test if a specific minor diagonal on this board contains a conflict
     hasMinorDiagonalConflictAt: function (minorDiagonalColumnIndexAtFirstRow) {
-      return false; // fixme
+      var rowsArr = this.rows();
+      var arr = [];
+      var j = minorDiagonalColumnIndexAtFirstRow;
+      for (var i = 0; i < this.rows().length; i++) {
+        arr.push(rowsArr[i][j]);
+        j--;
+      }
+      return arr.filter(function (val) {
+        return val === 1;
+      }).length > 1 ? true : false;
+
     },
 
     // test if any minor diagonals on this board contain conflicts
     hasAnyMinorDiagonalConflicts: function () {
+      for (var i = 0; i < this.rows().length; i++) {
+        if (hasMinorDiagonalConflictAt(i)) {
+          return true;
+        }
+      }
       return false; // fixme
+      //return false; // fixme
     }
 
     /*--------------------  End of Helper Functions  ---------------------*/
